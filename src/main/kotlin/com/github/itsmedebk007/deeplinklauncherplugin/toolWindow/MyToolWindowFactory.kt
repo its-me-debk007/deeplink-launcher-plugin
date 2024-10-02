9f133866d18fe8fbbc85173cefc68f99c77cdac9
Debashish Kundu <debashish.joy@gmail.com>
2024-10-02 21:22:59 +0530

    add coroutine scope

--- src/main/kotlin/com/github/itsmedebk007/deeplinklauncherplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/itsmedebk007/deeplinklauncherplugin/toolWindow/MyToolWindowFactory.kt
@@ -4,8 +4,11 @@ import com.android.ddmlib.IDevice
 import com.android.tools.idea.wearpairing.runShellCommand
 import com.github.itsmedebk007.deeplinklauncherplugin.services.DataPersistentComponent
 import com.github.itsmedebk007.deeplinklauncherplugin.util.NotificationUtil
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.service
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBCheckBox
@@ -13,9 +16,10 @@ import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.content.ContentFactory
-import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import org.jetbrains.android.sdk.AndroidSdkUtils
 import java.awt.Font
@@ -23,7 +27,7 @@ import java.awt.event.ItemEvent
 import java.net.URI
 import javax.swing.JButton
 
-class MyToolWindowFactory : ToolWindowFactory {
+class MyToolWindowFactory : ToolWindowFactory, DumbAware {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
@@ -38,7 +42,12 @@ class MyToolWindowFactory : ToolWindowFactory {
         private val dataPersistentComponent = toolWindow.project.service<DataPersistentComponent>()
         private val devicesList = mutableListOf<IDevice>()
 
-        fun getContent() = JBPanel<JBPanel<*>>().apply {
+        fun getContent() = JBPanel<JBPanel<*>>(FlowLayout(FlowLayout.LEFT)).apply {
+            val customCoroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+            Disposer.register(toolWindow.disposable, Disposable {
+                customCoroutineScope.cancel()
+            })
+
             val deeplinkList = dataPersistentComponent.state
 
             val heading = JBLabel("Enter deeplink here").apply {
@@ -50,9 +59,23 @@ class MyToolWindowFactory : ToolWindowFactory {
                 addActionListener {
 //                    val facets = ProjectFacetManager.getInstance(toolWindow.project).getFacets(AndroidFacet.ID)
 //                    val packageName = AndroidModel.get(facets[0])?.applicationId
-                    devicesList.launchDeepLink(textField.text)
-                    deeplinkList.deeplinks.add(textField.text)
-                    dataPersistentComponent.loadState(deeplinkList)
+                    customCoroutineScope.launch {
+                        devicesList.launchDeepLink(
+                            deeplink = textField.text,
+                            onSuccess = {
+                                NotificationUtil.showInfoNotification("Deeplink launched successfully")
+                                if (deeplinkList.deeplinks.contains(textField.text).not()) {
+                                    deeplinkList.deeplinks.add(textField.text)
+                                    dataPersistentComponent.loadState(deeplinkList)
+                                    listModel.addElement(textField.text)
+                                }
+                                textField.text = ""
+                            },
+                            onFailure = {
+                                NotificationUtil.showErrorNotification("Incorrect deeplink")
+                            }
+                        )
+                    }
                 }
             }
 
@@ -84,11 +107,11 @@ class MyToolWindowFactory : ToolWindowFactory {
                     }
                 }
 
-                add(deeplinkBtn)
-            }
-        }
-
-        private fun List<IDevice>.launchDeepLink(deeplink: String) {
+        private suspend fun List<IDevice>.launchDeepLink(
+            deeplink: String,
+            onSuccess: () -> Unit,
+            onFailure: () -> Unit
+        ) {
             if (deeplink.isBlank()) return
 
             runCatching { URI(deeplink.trim()) }
